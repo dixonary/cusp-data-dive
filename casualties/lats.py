61effd7aa7d5eef0639fee66c0468208c5fe170b
Alex Dixon <dixonary@martin.local>
2018-03-13 11:44:36 +0000

    added casualty lonlat conversion

--- casualties/lats.py
+++ casualties/lats.py
@@ -4,12 +4,18 @@ import pandas
 from convertbng.util import convert_lonlat
 
 def lats(easting, northing):
-    (longitude, latitude) = convert_lonlat(easting, northing, 30, 'U');
+    (longitudes, latitudes) = convert_lonlat(easting, northing);
     return latitudes;
 
 def lons(easting, northing):
-    (longitudes, latitudes) = convert_lonlat(easting, northing, 30, 'U');
+    (longitudes, latitudes) = convert_lonlat(easting, northing);
     return longitudes;
 
 data = pandas.read_csv('casualty_points.csv');
-data['longitude'] = 
+data['longitude'] = lons(data['Easting'], data['Northing']);
+data['latitude']  = lats(data['Easting'], data['Northing']);
+
+print(data)
+
+data.to_csv('casualty_points_lonlat.csv', columns=('longitude', 'latitude'),
+        index=False)
